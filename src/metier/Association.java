4801108bd5ed3142af3aeb1ee65b6e1dc2bb596e
Thibault Mangeant <166180018+ThibaultMangeant@users.noreply.github.com>
2024-12-10 15:15:18 +0100

    Amélioration de la qualité du code de Association

--- src/metier/Association.java
+++ src/metier/Association.java
@@ -3,84 +3,96 @@ package metier;
 import java.util.ArrayList;
 import java.util.List;
 
-import metier.Notion;
-import metier.Ressource;
-
-public class Association extends Question {
-
-	/*---------*/
-	/* Données */
-	/*---------*/
-
+public class Association extends Question
+{
+	/* Attributs */
 	private List<List<String>> proposition;
 
-	/*-----------*/
-	/* Structure */
-	/*-----------*/
+	/*--------------*/
+	/* Constructeur */
+	/*--------------*/
 
 	public Association(	int id, String intitule, String explication, String difficulte,
-						Ressource ressource, Notion notion, int temps, int note) {
-
-			super(id, intitule, explication, difficulte, ressource, notion, temps, note);
-			proposition = new ArrayList<>();
+						Ressource ressource, Notion notion, int temps, int note)
+	{
+		super(id, intitule, explication, difficulte, ressource, notion, temps, note);
+		this.proposition = new ArrayList<>();
 	}
 
-	/*---------*/
-	/* Getteur */
-	public List<List<String>> getProposition()	{ return this.proposition; }
+	/*----------*/
+	/* Getteurs */
+	/*----------*/
 
-	/* Ajouter une association (Proposition, Réponse)*/
-	public boolean ajouterAssociation(String newProposition, String newReponse) {
-		for(List<String> proposition : this.proposition) {
-			if(proposition.get(0) == newProposition) { return false; }	// Si la proposition existe déjà 	-> renvoie faux
-			if(proposition.get(1) == newReponse)     { return false; }	// Si la réponse existe déjà 		-> renvoie faux
+	public List<List<String>> getProposition() { return this.proposition; }
+
+	/* Ajouter une association (Proposition, Réponse) */
+	public boolean ajouterAssociation(String newProposition, String newReponse)
+	{
+		List<String> association;
+
+
+		for(List<String> proposition : this.proposition)
+		{
+			if(proposition.get(0) == newProposition) { return false; } // Si la proposition existe déjà -> renvoie faux
+			if(proposition.get(1) == newReponse)     { return false; } // Si la réponse existe déjà     -> renvoie faux
 		}
 
-		List<String> association = new ArrayList<>();	//Création de l'association (Proposition, Réponse)
+		association = new ArrayList<String>(); // Création de l'association (Proposition, Réponse)
 		association.add(newProposition);
 		association.add(newReponse);
 
-		this.proposition.add(association);				// Ajout de l'association dans la liste des associations
+		this.proposition.add(association); // Ajout de l'association dans la liste des associations
 		return true;
 	}
 
 
 	/* Modifier */
-	public boolean modifierProposition(int id, String newProposition) {
-		if(id>this.proposition.size() || id<0)	{ return false; }	// Si l'id est en dehors de la liste				-> renvoie faux
-		if(this.proposition.isEmpty())			{ return false; }	// Si la liste est vide								-> renvoie faux
-		if(this.proposition.get(id).isEmpty())	{ return false; }	// Si l'association n'éxiste pas					-> renvoie faux
+	public boolean modifierProposition(int id, String newProposition)
+	{
+		List<String> association;
 
-		for(List<String> proposition : this.proposition) {
-			if(!this.proposition.get(id).equals(newProposition)) { return false; }	// Si il n'y a pas de modificatiomn	-> renvoie faux
+
+		if(id > this.proposition.size() || id < 0)  { return false; } // Si l'id est en dehors de la liste -> renvoie faux
+		if(this.proposition.isEmpty())              { return false; } // Si la liste est vide              -> renvoie faux
+		if(this.proposition.get(id).isEmpty())      { return false; } // Si l'association n'éxiste pas     -> renvoie faux
+
+		for(List<String> proposition : this.proposition)
+		{
+			if ( !proposition.get(0).equals(newProposition) ) { return false; } // Si il n'y a pas de modification -> renvoie faux
 		}
 
-		List<String> association = this.proposition.get(id);	// Récupération de l'association
-		association.set(0, newProposition);				// Modification de Proposition dans l'association
-		this.proposition.set(id, association);					// Modifications dans la liste des associations avec l'association modifiée
+		association = this.proposition.get(id);   // Récupération de l'association
+		association.set(0, newProposition); // Modification de Proposition dans l'association
+		this.proposition.set(id, association);    // Modifications dans la liste des associations avec l'association modifiée
 		return true;
 	}
 
-	public boolean modifierReponse(int id, String newReponse) {
-		if(id>this.proposition.size() || id<0)	{ return false; }	// Si l'id est en dehors de la liste				-> renvoie faux
-		if(this.proposition.isEmpty())			{ return false; }	// Si la liste est vide					-> renvoie faux
-		if(this.proposition.get(id).isEmpty())	{ return false; }	// Si l'association n'éxiste pas					-> renvoie faux
+	public boolean modifierReponse(int id, String newReponse)
+	{
+		List<String> association;
 
-		for(List<String> proposition : this.proposition) {
-			if(this.proposition.get(id).equals(newReponse)) { return false; }	// Si il n'y a pas de modification		-> renvoie faux
+
+		if(id>this.proposition.size() || id<0)	{ return false; } // Si l'id est en dehors de la liste -> renvoie faux
+		if(this.proposition.isEmpty())			{ return false; } // Si la liste est vide              -> renvoie faux
+		if(this.proposition.get(id).isEmpty())	{ return false; } // Si l'association n'éxiste pas     -> renvoie faux
+
+		for(List<String> proposition : this.proposition)
+		{
+			if(proposition.get(0).equals(newReponse)) { return false; } // Si il n'y a pas de modification -> renvoie faux
 		}
 
-		List<String> association = this.proposition.get(id);	// Récupération de l'association
-		association.set(1, newReponse);					// Modification de Réponse dans l'association
-		this.proposition.set(id, association);					// Modifications dans la liste des associations avec l'association modifiée
+		association = this.proposition.get(id); // Récupération de l'association
+		association.set(1, newReponse);   // Modification de Réponse dans l'association
+		this.proposition.set(id, association);  // Modifications dans la liste des associations avec l'association modifiée
 		return true;
 	}
 
 
 	/* Ajouter une association (Proposition, Réponse) */
-	public boolean supprimerAssociation(int id) {
-		if(id>this.proposition.size() || id<0)	{ return false; }	// Si l'id est en dehors de la liste	-> renvoie faux
-		if(this.proposition.isEmpty())			{ return false; }	// Si la liste est vide					-> renvoie faux
+	public boolean supprimerAssociation(int id)
+	{
+		if(id>this.proposition.size() || id<0)  { return false; } // Si l'id est en dehors de la liste -> renvoie faux
+		if(this.proposition.isEmpty())          { return false; } // Si la liste est vide              -> renvoie faux
 
 		this.proposition.remove(id);
 		return true;
@@ -88,20 +100,26 @@ public class Association extends Question {
 
 
 	/* toString */
-	public String toString() {
+	public String toString()
+	{
 		String result = "";
 
 		if(this.proposition.isEmpty()) { return "Il n'y a pas de propostions"; }
 
-		for (List<String> proposition : this.proposition) {
+		for (List<String> proposition : this.proposition)
+		{
 			result += proposition.get(0) + " --> " + proposition.get(1) + "\n"; 
 		}
 
 		return result;
 	}
 
-	public static void main(String[] args) {
-		Association asso = new Association(0, null, null, null, null, null, 0, 0);
+	public static void main(String[] args)
+	{
+		Association asso, asso2;
+
+
+		asso = new Association(0, null, null, null, null, null, 0, 0);
 		asso.ajouterAssociation("Prop1", "Rep1");
 		asso.ajouterAssociation("Prop2", "Rep2");
 		System.out.println(asso);
@@ -112,12 +130,11 @@ public class Association extends Question {
 
 		asso.supprimerAssociation(1);
 		asso.supprimerAssociation(-1);
-		asso.supprimerAssociation(4);	// Ne le fait pas car en dehors de la liste
+		asso.supprimerAssociation(4); // Ne le fait pas car en dehors de la liste
 		System.out.println(asso);
 
-		Association asso2 = new Association(0, null, null, null, null, null, 0, 0);
+		asso2 = new Association(0, null, null, null, null, null, 0, 0);
 		asso2.supprimerAssociation(0);
-
 	}
 
 }
