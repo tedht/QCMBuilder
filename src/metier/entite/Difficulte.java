9f4cb77ed5945739d1e8c1e3ff8ce3add6e08e7f
Thibault Mangeant <166180018+ThibaultMangeant@users.noreply.github.com>
2024-12-11 16:27:12 +0100

    Ajout Enum Difficulte

---
+++ src/metier/entite/Difficulte.java
@@ -0,0 +1,33 @@
+package metier.entite;
+
+public enum Difficulte
+{
+	TRES_FACILE("TF"),
+	FACILE     ("F" ),
+	MOYEN      ("M" ),
+	DIFFICILE  ("D" );
+
+
+	private String difficulte;
+
+	// Constructeur
+	Difficulte(String difficulte)
+	{
+		this.difficulte = difficulte;
+	}
+
+	// Getters
+	public String getDifficulte() { return this.difficulte; }
+
+	public String toString()
+	{
+		return "Difficulte : " + this.difficulte;
+	}
+
+	public static void main(String[] args)
+	{
+		Difficulte d = Difficulte.TRES_FACILE;
+		System.out.println(d.getDifficulte());
+		System.out.println(d);
+	}
+}
