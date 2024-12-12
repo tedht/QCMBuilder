b489085b6c2bd018ee088f8cc0e9e4843da8c69e
Ted Herambert <tedherambert2004@gmail.com>
2024-12-12 08:55:03 +0100

    Modification de l'enum TypeQuestion

--- src/metier/entite/question/TypeQuestion.java
+++ src/metier/entite/question/TypeQuestion.java
@@ -1,21 +1,76 @@
 package metier.entite.question;
 
+/**
+ * Enumération représentant les types de questions.
+ * Cette énumération inclut des méthodes utilitaires pour manipuler et convertir les types de questions.
+ * 
+ * @author Equipe 03
+ * @date 2024/12/12
+ * @version 1.0
+ */
 public enum TypeQuestion 
 {
-	QCM        ("QCM"),
-	ASSOCIATION("Association" ),
-	ELIMINATION("Elimination" );
+	QCM        (0),
+	ASSOCIATION(1),
+	ELIMINATION(2);
 
-	private String type;
+	private final int valeur;
 
-	// Constructeur
-	private TypeQuestion(String type)
+	/**
+     * Constructeur privé de l'énumération.
+     * 
+     * @param valeur La valeur entière associée au type de question.
+     */
+	private TypeQuestion(int valeur)
 	{
-		this.type = type;
+		this.valeur = valeur;
 	}
 
+	/**
+     * Obtient la valeur entière associée à ce type de question.
+     * 
+     * @return La valeur entière du type.
+     */
+    public int getValeur() 
+    { 
+        return this.valeur; 
+    }
+
+	/**
+     * Convertit une valeur entière en un type de question.
+     * 
+     * @param valeur La valeur entière.
+     * @return Le type de question correspondant.
+     * @throws IllegalArgumentException Si la valeur fournie ne correspond à aucun type.
+     */
+    public static TypeQuestion fromInt(int valeur) 
+    {
+        // Parcours de toutes les valeurs de l'énumération pour trouver une correspondance.
+        for (TypeQuestion type : TypeQuestion.values()) 
+        {
+            if (type.getValeur() == valeur) 
+            { 
+                return type; 
+            }
+        }
+
+        // Lancer une exception si la valeur n'est pas valide.
+        throw new IllegalArgumentException("Valeur difficulté non valide : " + valeur);
+    }
+
+	/**
+     * Retourne une représentation en chaîne de caractères de la difficulté.
+     * 
+     * @return une représentation en chaîne de caractères de la difficulté.
+     */
 	public String toString()
 	{
-		return this.type;
+		return switch(this.valeur) 
+		{
+			case 0  -> "QCM";
+			case 1  -> "Association";
+			case 2  -> "Elimination";
+			default -> "";
+		};
 	}
 }
