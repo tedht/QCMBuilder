bba6b2837ca52a2fe1145ee71a7be9ec6b484114
Ted Herambert <tedherambert2004@gmail.com>
2024-12-12 08:50:44 +0100

    Modification de l'enum Difficulte

--- src/metier/entite/question/Difficulte.java
+++ src/metier/entite/question/Difficulte.java
@@ -1,23 +1,79 @@
 package metier.entite.question;
 
+/**
+ * Enumération représentant les difficultés.
+ * Cette énumération inclut des méthodes utilitaires pour manipuler et convertir les difficultés.
+ * 
+ * @author Equipe 03
+ * @date 2024/12/12
+ * @version 1.0
+ */
 public enum Difficulte
 {
-	TRES_FACILE("TF"),
-	FACILE     ("F" ),
-	MOYEN      ("M" ),
-	DIFFICILE  ("D" );
+	TRES_FACILE(0),
+	FACILE     (1),
+	MOYEN      (2),
+	DIFFICILE  (3);
 
-	private String difficulte;
+	private final int valeur;
 
-	// Constructeur
-	private Difficulte(String difficulte)
+	/**
+     * Constructeur privé de l'énumération.
+     * 
+     * @param valeur La valeur entière associée à la difficulté.
+     */
+	private Difficulte(int valeur)
 	{
-		this.difficulte = difficulte;
+		this.valeur = valeur;
 	}
 
+	/**
+     * Obtient la valeur entière associée à cette difficulté.
+     * 
+     * @return La valeur entière de la difficulté.
+     */
+    public int getValeur() 
+    { 
+        return this.valeur; 
+    }
+
+	/**
+     * Convertit une valeur entière en une difficulté.
+     * 
+     * @param valeur La valeur entière.
+     * @return La difficulté correspondante.
+     * @throws IllegalArgumentException Si la valeur fournie ne correspond à aucune difficulté.
+     */
+    public static Difficulte fromInt(int valeur) 
+    {
+        // Parcours de toutes les valeurs de l'énumération pour trouver une correspondance.
+        for (Difficulte difficulte : Difficulte.values()) 
+        {
+            if (difficulte.getValeur() == valeur) 
+            { 
+                return difficulte; 
+            }
+        }
+
+        // Lancer une exception si la valeur n'est pas valide.
+        throw new IllegalArgumentException("Valeur difficulté non valide : " + valeur);
+    }
+
+	/**
+     * Retourne une représentation en chaîne de caractères de la difficulté.
+     * 
+     * @return une représentation en chaîne de caractères de la difficulté.
+     */
 	public String toString()
 	{
-		return this.difficulte;
+		return switch(this.valeur) 
+		{
+			case 0  -> "TF";
+			case 1  -> "F";
+			case 2  -> "M";
+			case 3  -> "D";
+			default -> "";
+		};
 	}
 
 	public static void main(String[] args)
