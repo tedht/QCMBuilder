d14b6969ccf788594e0c8af785627b5a0bf8791d
Kylian LucasThuillier <kylian.lucast@gmail.com>
2024-12-17 10:56:08 +0100

    Réarrangement Controlleur

--- src/controleur/Controleur.java
+++ src/controleur/Controleur.java
@@ -3,116 +3,249 @@ package controleur;
 import java.util.List;
 
 import ihm.IHM;
-
 import metier.entite.Ressource;
 import metier.entite.Notion;
-
 import metier.entite.question.Question;
-
 import metier.QCMBuilder;
 
+/**
+ * Classe Controleur.
+ * 
+ * @author  Equipe 3.
+ * @version 1.0 17/12/2024.
+ */
 public class Controleur
 {
+
+
+
+	/*-----------*/
+	// Attributs //
+	/*-----------*/
 	private QCMBuilder metier;
 	
+
+
+	/*--------------*/
+	/* Constructeur */
+	/*--------------*/
+
+	/**
+	 * Constructeur de la classe Controleur.
+	 */
 	public Controleur()
 	{
 		this.metier = new QCMBuilder();
 		new IHM(this);
 	}
 
-	public static void main(String[] args) 
-	{
-		new Controleur();
-	}
 
+
+	/*---------*/
+	// Getters //
+	/*---------*/
+
+	/**
+	 * Retourne la liste des ressources.
+	 * 
+	 * @return la liste des ressources.
+	 */
 	public List<Ressource> getRessources()
 	{
 		return this.metier.getRessources();
 	}
 
-	public List<Notion> getNotions(Ressource ressource)
-	{
-		return this.metier.getNotions(ressource);
-	}
-
-	public List<Question> getQuestions(Ressource ressource, Notion notion)
-	{
-		return this.metier.getQuestions(ressource, notion);
-	}
-
-	public void setRessourceActive(Ressource ressource) 
-	{
-		this.metier.setRessourceActive(ressource);
-	}
-
-	public void setNotionActive(Notion notion) 
-	{
-		this.metier.setNotionActive(notion);
-	}
-
-	public int getNbNotions(Ressource ressource) 
-	{
-		return this.getNotions(ressource).size();
-	}
-
-	public int getNbQuestions(Ressource ressource, Notion notion) 
-	{
-		return this.getQuestions(ressource, notion).size();
-	}
-
-	public Ressource getRessourceActive()
-	{
-		return this.metier.getRessourceActive();
-	}
-
-	public Notion getNotionActive()
-	{
-		return this.metier.getNotionActive();
-	}
-
-	public void creerRessource(String nomRessource) 
-	{
-		this.metier.creerRessource(nomRessource);
-	}
-
-	public void creerNotion(String nomNotion)  
-	{
-		this.metier.creerNotion(nomNotion);
-	}
-
-	public void creerQuestion() 
-	{
-		this.metier.creerQuestion();
-	}
-
-	public void popHistorique() 
-	{
-		this.metier.popHistorique();
-	}
-
+	/**
+	 * Retourne une ressource à partir de son nom.
+	 * 
+	 * @param  nomRessource le nom de la ressource.
+	 * @return              la ressource
+	 */
 	public Ressource getRessource(String nomRessource) 
 	{
 		return this.metier.getRessource(nomRessource);
 	}
 
+	/**
+	 * Retourne la liste des notions associées à une ressource.
+	 * 
+	 * @param  ressource la ressource.
+	 * @return           la liste des notions.
+	 */
+	public List<Notion> getNotions(Ressource ressource)
+	{
+		return this.metier.getNotions(ressource);
+	}
+
+	/**
+	 * Retourne la liste des questions associées à une notion associée à une ressource.
+	 * 
+	 * @param  ressource la ressource.
+	 * @param  notion    la notion.
+	 * @return           la liste des questions.
+	 */
+	public List<Question> getQuestions(Ressource ressource, Notion notion)
+	{
+		return this.metier.getQuestions(ressource, notion);
+	}
+
+	/**
+	 * Retourne le nombre de notions associées à une ressource.
+	 * 
+	 * @param  ressource la ressource.
+	 * @return           le nombre de notions.
+	 */
+	public int getNbNotions(Ressource ressource) 
+	{
+		return this.getNotions(ressource).size();
+	}
+
+	/**
+	 * Retourne le nombre de questions associées à une notion associée à une ressource.
+	 * 
+	 * @param  ressource la ressource.
+	 * @param  notion    la notion.
+	 * @return           le nombre de questions.
+	 */
+	public int getNbQuestions(Ressource ressource, Notion notion) 
+	{
+		return this.getQuestions(ressource, notion).size();
+	}
+
+	/**
+	 * Retourne la ressource active.
+	 * 
+	 * @return la ressource active.
+	 */
+	public Ressource getRessourceActive()
+	{
+		return this.metier.getRessourceActive();
+	}
+
+	/**
+	 * Retourne la notion active.
+	 * 
+	 * @return la notion active.
+	 */
+	public Notion getNotionActive()
+	{
+		return this.metier.getNotionActive();
+	}
+
+
+
+	/*---------*/
+	// Setters //
+	/*---------*/
+
+	/**
+	 * Modifie la ressource active.
+	 * 
+	 * @param ressource la nouvelle ressource active.
+	 */
+	public void setRessourceActive(Ressource ressource) 
+	{
+		this.metier.setRessourceActive(ressource);
+	}
+
+	/**
+	 * Modifie la notion active.
+	 * 
+	 * @param notion la nouvelle notion active.
+	 */
+	public void setNotionActive(Notion notion) 
+	{
+		this.metier.setNotionActive(notion);
+	}
+
+
+
+	/*-----------------*/
+	// Autres méthodes //
+	/*-----------------*/
+
+	/**
+	 * Creer une nouvelle ressource.
+	 * 
+	 * @param nomRessource le nom de la nouvelle ressource.
+	 */
+	public void creerRessource(String nomRessource) 
+	{
+		this.metier.creerRessource(nomRessource);
+	}
+
+	/**
+	 * Creer une nouvelle notion.
+	 * 
+	 * @param nomNotion le nom de la nouvelle notion.
+	 */
+	public void creerNotion(String nomNotion)  
+	{
+		this.metier.creerNotion(nomNotion);
+	}
+
+	/**
+	 * Creer une Question.
+	 */
+	public void creerQuestion() 
+	{
+		this.metier.creerQuestion();
+	}
+
+	/**
+	 * 
+	 */
+	public void popHistorique() 
+	{
+		this.metier.popHistorique();
+	}
+
+	/**
+	 * Creer une question QCM.
+	 * 
+	 * @param  detailsQuestion toutes les informations de la questions.
+	 * @param  unique          true si le qcm est à réponse unique, false si il est à réponse multiples.
+	 * @return                 la liste des érreures ayant pu être trouvé lors de la création de la question.
+	 */
 	public List<String> creerQCM(String detailsQuestion, boolean unique) 
 	{
 		return this.metier.creerQCM(detailsQuestion, unique);
 	}
 
+	/**
+	 * Creer une question Association.
+	 * 
+	 * @param  detailsQuestion toutes les informations de la questions.
+	 * @return                 la liste des érreures ayant pu être trouvé lors de la création de la question.
+	 */
 	public List<String> creerAssociation(String detailsQuestion) 
 	{
 		return this.metier.creerAssociation(detailsQuestion);
 	}
 
+	/**
+	 * Creer une question Elimination.
+	 * 
+	 * @param  detailsQuestion toutes les informations de la questions.
+	 * @return                 la liste des érreures ayant pu être trouvé lors de la création de la question.
+	 */
 	public List<String> creerElimination(String detailsQuestion) 
 	{
 		return this.metier.creerElimination(detailsQuestion);
 	}
 
-	public void genererEvaluation(String cheminFichier)
+	/**
+	 * Génére une évaluation.
+	 */
+	public void genererEvaluation() 
 	{
-		this.metier.genererEvaluation(cheminFichier);
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException("Unimplemented method 'genererEvaluation'");
+	}
+
+
+	public static void main(String[] args) 
+	{
+		new Controleur();
 	}
 }
