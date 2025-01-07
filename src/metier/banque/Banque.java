c5de8d959e960bf4b4d7ce0a8b01b307718ce197
Thibault Mangeant <166180018+ThibaultMangeant@users.noreply.github.com>
2025-01-07 09:49:09 +0100

    Javadoc + Qualité Banque.java

--- src/metier/banque/Banque.java
+++ src/metier/banque/Banque.java
@@ -2,20 +2,21 @@ package metier.banque;
 
 import java.io.File;
 
+/**
+ * Classe abstraite Banque.
+ * 
+ * @version 1.0 du 2024-12-09 Norme ISO-8601
+ */
 public abstract class Banque 
 {
 	public boolean supprimerDossier(File dossier)
 	{
-		if (dossier.isDirectory()) {
-            for (File file : dossier.listFiles()) 
-			{
-                if (!this.supprimerDossier(file)) 
-				{
-                    return false;
-                }
-            }
-        }
+		if (dossier.isDirectory())
+			for (File file : dossier.listFiles())
+				if (!this.supprimerDossier(file))
+					return false;
 
-        return dossier.delete();
+
+		return dossier.delete();
 	}
 }
